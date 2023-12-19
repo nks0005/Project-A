037d470da680c1627d8aa1cb46979ebc50b2402a
AnHyoungBin <nks0005@naver.com>
2023-12-19 19:23:38 +0900

    Create crawler.js

---
+++ src/util/crawler.js
@@ -0,0 +1,21 @@
+const fetchDataFromUrl = async (url) => {
+    try {
+        const response = await axios.get(url);
+        const statusGroup = Math.floor(response.status / 100);
+
+        if (statusGroup === 4 || statusGroup === 5) {
+            console.error(`Error status code from ${url}: ${response.status}`);
+            return null;
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching data from ${url}: ${error}`);
+        return null;
+    }
+}
+
+
+module.exports = {
+    fetchDataFromUrl
+};
